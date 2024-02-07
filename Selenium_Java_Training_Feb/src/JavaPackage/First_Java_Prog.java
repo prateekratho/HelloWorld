821b180ceec45f1d10d68a8ad847898076891d5e
prateekrath <prateekrathod2493@gmail.com>
2024-02-07 23:05:45 +0530

    Initial Commit

---
+++ Selenium_Java_Training_Feb/src/JavaPackage/First_Java_Prog.java
@@ -0,0 +1,8 @@
+package JavaPackage;
+
+public class First_Java_Prog {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Selenium Java Training");
+	}
+
+}
---
+++ Selenium_Java_Training_Feb/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Selenium_Java_Training_Feb {
+}
