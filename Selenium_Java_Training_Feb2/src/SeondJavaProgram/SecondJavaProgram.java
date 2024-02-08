8cc2d4153837801bdf96f4205c9c3b6456624377
prateekrath <prateekrathod2493@gmail.com>
2024-02-08 09:27:09 +0530

    Committed Code

---
+++ Selenium_Java_Training_Feb2/src/SeondJavaProgram/SecondJavaProgram.java
@@ -0,0 +1,10 @@
+package SeondJavaProgram;
+
+public class SecondJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("Second Program Code");
+		System.out.println("Good Afternoon");
+	}
+	
+
+}
