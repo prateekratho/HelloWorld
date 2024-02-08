282e5413e17837081eb1ead1e010263387df8d86
prateekrath <prateekrathod2493@gmail.com>
2024-02-08 09:08:50 +0530

    Updated the code

--- Selenium_Java_Training_Feb/src/JavaPackage/First_Java_Prog.java
+++ Selenium_Java_Training_Feb/src/JavaPackage/First_Java_Prog.java
@@ -3,6 +3,7 @@ package JavaPackage;
 public class First_Java_Prog {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Selenium Java Training");
+		System.out.println("Good Morning");
 	}
 
 }
