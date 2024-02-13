c0418b6fe214dd29519fdf40259e5a9cbeac97aa
prateekrath <prateekrathod2493@gmail.com>
2024-02-13 19:25:23 +0530

    conditionaloperators

--- Selenium_Java_Training_Feb/src/JavaPackage/conditionalStatement.java
+++ Selenium_Java_Training_Feb/src/JavaPackage/conditionalStatement.java
@@ -20,16 +20,26 @@ public class conditionalStatement {
 //		else {
 //			System.out.println("Number is Odd");
 //		}
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		if (a > b) {
-			System.out.println("a is greater");
-		} else if (a < b) {
-			System.out.println("b is greater");
-		} else {
-			System.out.println("Equal");
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		if (a > b) {
+//			System.out.println("a is greater");
+//		} else if (a < b) {
+//			System.out.println("b is greater");
+//		} else {
+//			System.out.println("Equal");
+//		}
+//		sc.close();
+		int button = sc.nextInt();
+		switch(button) {
+		case 1: System.out.println("Hello");
+		break;
+		case 2: System.out.println("Namaste");
+		break;
+		case 3: System.out.println("Bonjour");
+		break;
+		default: System.out.println("Invalid Button");
 		}
-		sc.close();
 	}
 
 }
---
+++ Selenium_Java_Training_Feb/src/JavaPackage/loopsconcept.java
@@ -0,0 +1,37 @@
+package JavaPackage;
+
+import java.util.Scanner;
+
+public class loopsconcept {
+
+	public static void main(String[] args) {
+//		for(int i=0; i<=3; i++){
+//		System.out.println("Hello World");
+//		}
+//		for (int i= 0; i<11; i++) {
+//			System.out.println(i+" ");
+//		} for loop
+//		int i = 0;
+//		while(i < 11) {
+//			System.out.println(i);
+//			i++;
+//		} while loop
+		 
+//		int i=0;
+//		do {
+//			System.out.println(i);
+//			i =i+1;
+//		}while(i<=11);
+		
+		//print sum of first n Natural Number n=4
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		//int sum = 0;
+		for (int i=1; i<=10 ; i++) {
+			System.out.println(n*i);
+		}
+	//	System.out.println(sum);
+		
+	}
+
+}
