35e7ab763a579437aba9b7603340e8d1c33249f9
prateekrath <prateekrathod2493@gmail.com>
2024-02-13 17:50:14 +0530

    practice code

--- Selenium_Java_Training_Feb/src/JavaPackage/FirstJavaProgram.java
+++ Selenium_Java_Training_Feb/src/JavaPackage/FirstJavaProgram.java
@@ -1,5 +1,20 @@
 package JavaPackage;
 
+import java.util.*;
+
 public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		//Input
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a+b;
+		System.out.println(sum);
+		System.out.println(sum);
+		//String name = sc.next();
+		//System.out.println(name);
+		//sc.close();
+	}
 
 }
---
+++ Selenium_Java_Training_Feb/src/JavaPackage/conditionalStatement.java
@@ -0,0 +1,35 @@
+package JavaPackage;
+
+import java.util.Scanner;
+
+public class conditionalStatement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int age = sc.nextInt();
+//		if (age > 18) {
+//			System.out.println("Adult");
+//		}else{
+//			System.out.println("Not Adult");
+//		}
+//		int num = sc.nextInt();
+//		System.out.println(num);
+//		if(num % 2 == 0) {
+//			System.out.println("Number is Even");
+//		}
+//		else {
+//			System.out.println("Number is Odd");
+//		}
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if (a > b) {
+			System.out.println("a is greater");
+		} else if (a < b) {
+			System.out.println("b is greater");
+		} else {
+			System.out.println("Equal");
+		}
+		sc.close();
+	}
+
+}
